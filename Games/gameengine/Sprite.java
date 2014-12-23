8e0382ba5db8ae5162b7217cae8af5fe77fd8a41
ravzac14 <raver_zack@yahoo.com>
2014-12-22 16:12:14 -0800

    Made a game engine with javafx, next step tryin to use it with tetris

---
+++ Games/gameengine/Sprite.java
@@ -0,0 +1,40 @@
+/* This is the basic framework of a sprite object
+ * to be used in the game engine
+ *
+ * @author  Zack Raver
+ * @date    12/21/14
+ * 
+ * These are supposed to be displayed
+ */
+
+package gameengine;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.animation.Animation;
+import javafx.scene.Node;
+
+public abstract class Sprite {
+
+    //Animations for the node
+    public List animations = new ArrayList<>();
+
+    //Current display node
+    public Node node;
+
+    //velocity vector x direction
+    public double vX = 0;
+
+    //velocity vector y direction
+    public double vY = 0;
+
+    //is dead?
+    public boolean isDead = false;
+
+    //updates the sprite objects velocity or animations
+    public abstract void update();
+
+    //did this sprite collide with another sprite
+    public boolean collide(Sprite other){ return false; }
+
+}
---
+++ Games/tetris/TetrisWorld.java
@@ -0,0 +1,29 @@
+/* Tetris main class..
+ *  - Has stuff
+ *
+ * @author  Zack Raver
+ * @date    12/21/14
+ * 
+ * This is my first crack at a game..sorry about the mess (im assuming)
+ */
+ 
+ package zjr.games.tetris;
+
+ import java.util.Random;
+
+
+ 
+ public class TetrisWorld extends GameWorld {
+    
+    //overarching game speed..controls how fast they descend (goes up over time)
+    public final static int SPEED;
+    
+    //Constructin'
+    public TetrisWorld(final int fps, final String title){
+        super(fps,title);
+    }
+
+    //initialize the world 
+    public void initialize(
+}
+
