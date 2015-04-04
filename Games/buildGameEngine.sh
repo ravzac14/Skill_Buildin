424cd05574e2a35478a2c75ee398854a9d5f2b51
ravzac14 <raver_zack@yahoo.com>
2015-04-04 11:02:18 -0700

    Moving stuff, getting ready for spring quarter data structures

--- Games/buildGameEngine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Compiles all the files in gameengine
-
-javac gameengine/Sprite.java
-javac gameengine/SpriteManager.java
-javac gameengine/GameWorld.java
--- Games/buildTetris.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#compiles all the tetris files
-
-javac tetris/TetrisWorld.java
-javac tetris/Tetris.java
-javac tetris/Block.java
-javac tetris/DeadBlock.java
